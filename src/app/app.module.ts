3e725e61403ba7c374e3aac2a03d1fb6cb54deaa
hanipatel81 <hanipatel81@yahoo.com>
2017-11-24 12:19:16 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
+import { CribsService } from './cribs.service';
+import { HttpModule } from '@angular/http';
+import { AddListingFormComponent } from './add-listing-form/add-listing-form.component';
+import { UtilService } from './util.service';
+import { SortByPipe } from './sort-by.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent,
+    CribCardComponent,
+    AddListingFormComponent,
+    SortByPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CribsService, UtilService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
